7872f75c1b00e6e545c84a6edbd7e6354b6363ac
Gonzalo <Gonzalo@192.168.1.16>
2017-05-28 20:13:05 +0200

    Añadido Premios y Carrito

---
+++ webApuestas/WebContent/JS/carrito.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    console.log("Cookie: "+getCookie("carrito"))
+    var arrayIds = getCookie("carrito").split(',')
+    console.log("Array split:" + arrayIds)
+    var json = {
+        
+    }
+    for (i in arrayIds) {
+        json[i] = arrayIds[i]
+    }
+    console.log(json)
+    var jsonString = JSON.stringify(json)
+    console.log("JSON Stringify: "+ JSON.stringify(json))
+    $.ajax({
+        type: 'GET'
+        , url: 'services/server/getcarrito/' + jsonString
+        , success: function (data) {
+            var json = JSON.parse(data)
+            console.log(data)
+                /*for (i in json) {
+                    var id = json[i].id
+                    var name = json[i].name
+                    var stock = json[i].stock
+                    var price = json[i].price
+                    var image = json[i].image
+                    stock = comprobarStock(stock)
+                    createPresent(id, name, stock, price, image, button)
+                }*/
+        }
+    });
+    //createTr()
+})
+
+function createTr() {
+    $('#addRow').prepend('<tr> <td class="col-sm-8 col-md-6"><div class="media"> <a class ="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"></a><div class="media-body"><h4 class="media-heading"> <a href="#">Product name</a></h4><h5 class="media-heading">Ref: ' + id + '</h5><span> Status: </span><span class="text-success"><strong>In Stock</strong></span></div></div></td><td class="col-sm-1 col-md-1" style="text-align: center"><input type="email" class="form-control" id="exampleInputEmail1" value="3"></td><td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td><td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td><td class="col-sm-1 col-md-1"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span> Remove </button></td></tr>');
+}
+
+function comprobarStock(stock) {
+    var html = ""
+    switch (true) {
+    case stock > 10:
+        html = '<p class="stock stock-verde"> En Stock </p>'
+        break;
+    case stock <= 10 && stock > 0:
+        html = '<p class="stock stock-amarillo"> En Stock </p>'
+        break;
+    case stock <= 0:
+        html = '<p class="stock stock-rojo">No Stock</p>'
+        break;
+    }
+    return html
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
--- webApuestas/WebContent/JS/premios.js
+++ webApuestas/WebContent/JS/premios.js
@@ -1,19 +1,67 @@
+var carrito = []
 $(document).ready(function () {
     $.ajax({
         type: 'GET'
         , url: 'services/server/getpresents'
         , success: function (data) {
-        	var json = JSON.parse(data)
-            var contador = 0;
-            while (contador < json.length){
-                var id = json[contador].id	
-                var name = json[contador].name
-                var stock = json[contador].stock
-                var price = json[contador].price
-                var image = json[contador].image
-                $("#presents").append('<div class="col-xs-12 col-sm-6 col-md-3"><div class="col-item"><div class="post-img-content"> <img src="'+image+'" class="img-responsive" alt="a" /> </div><div class="info"><div class="row"><div class="price col-md-6"><h5>'+name+'</h5><h5 class="price-text-color">'+price+'Coins</h5> </div><div class="rating hidden-sm col-md-6"><p class="stock">'+stock+'</p></div></div><div class="separator clear-left"><p class="btn-add" id="limpia"> <i class="fa fa-shopping-cart"></i><a href="http://www.google.es" class="hidden-sm">Añadir al Carrito</a></p></div><div class="clearfix"></div></div></div></div>');
-                contador++
-                }
+            var json = JSON.parse(data)
+            for (i in json) {
+                var id = json[i].id
+                var name = json[i].name
+                var stock = json[i].stock
+                var price = json[i].price
+                var image = json[i].image
+                var button = printButtonAddCart(stock, id)
+                stock = comprobarStock(stock)
+                createPresent(id, name, stock, price, image, button)
+            }
         }
     });
+    $(document).on('click', '.btn-add', function () {
+        var present = $(this).attr("id")
+        carrito.push(present)
+        setCookie("carrito", carrito, 1)
+        console.log(document.cookie)
+        console.log(carrito)
+    });
 });
+
+function comprobarStock(stock) {
+    var html = ""
+    switch (true) {
+    case stock > 10:
+        html = '<p class="stock stock-verde"> En Stock </p>'
+        break;
+    case stock <= 10 && stock > 0:
+        html = '<p class="stock stock-amarillo"> En Stock </p>'
+        break;
+    case stock <= 0:
+        html = '<p class="stock stock-rojo">No Stock</p>'
+        break;
+    }
+    return html
+}
+
+function printButtonAddCart(stock, id) {
+    var html = ""
+    switch (true) {
+    case stock <= 0:
+        html = ''
+        break;
+    case stock > 0:
+        html = '<p class="btn-add" id="' + id + '"><i class="fa fa-shopping-cart"></i><a href="#" class="hidden-sm">Add to cart</a></p>'
+        break;
+    }
+    return html
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function createPresent(id, name, stock, price, image, button) {
+    $(".presents").append('<div class="col-xs-12 col-sm-6 col-md-3"><div class="col-item"><div class="post-img-content"> <img src="' + image + '" class="img-responsive" alt="a" /> </div><div class="info"><div class="row"><div class="price col-md-6"><h5>' + name + '</h5><h5 class="price-text-color">' + price + ' Coins</h5> </div><div class="rating hidden-sm col-md-6">' + stock + '</div></div><div class="separator clear-left">' + button + '<p class="btn-details">Num Ref: ' + id + '</p></div><div class="clearfix"></div></div></div> </div>');
+}
